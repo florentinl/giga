d54f9745f61afdeed4cd2e8ddbecf5598062dda5
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2023-05-10 13:38:14 +0200

    Editor

---
+++ src/editor.rs
@@ -0,0 +1,51 @@
+/// Editor structure
+/// represents the state of the program
+pub struct Editor {
+    content: Vec<Vec<u8>>,
+}
+
+impl Editor {
+    pub fn new() -> Self {
+        Self {
+            content: Vec::new(),
+        }
+    }
+
+    pub fn open(path: &str) -> Result<Self, std::io::Error> {
+        let content = std::fs::read(path)?;
+        let content = content.split(|&c| c == b'\n')
+            .map(|s| s.to_vec())
+            .collect();
+
+        Ok(Self {
+            content,
+        })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn editor_new_empty() {
+        let editor = Editor::new();
+        assert_eq!(editor.content.len(), 0);
+    }
+
+    #[test]
+    fn editor_open() {
+        let path = "tests/sample.txt";
+        let editor = Editor::open(path);
+        assert!(editor.is_ok());
+
+        let editor = editor.unwrap();
+
+        let expected = vec![
+            "Hello, World !".as_bytes(),
+            "".as_bytes(),
+        ];
+        assert_eq!(editor.content, expected);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod editor;
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() > 2 {
+        println!("Usage: giga [file]")
+    }
+
+    // Optional file to edit
+    let file: Option<&str> = args.get(1).map(|s| s.as_str());
 }
