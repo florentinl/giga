3d7a34b8fc012b44b9df0105e708b120e3c0df84
Gerf <73294402+GerfautGE@users.noreply.github.com>
2023-05-28 13:47:38 +0200

    init slidev

--- tests/test_c.c
+++
@@ -1,8 +0,0 @@
-// This is a dumb C file to test the syntax highlighting of C files.
-int main(int argc, char **argv) {
-    int i = 0;
-    for (i = 0; i < argc; i++) {
-        printf("Hello, %s!\n", argv[i]);
-    }
-    return 0;
-}
