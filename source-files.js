16f71425d879caab7787cd6ba0ea883df78f6254
florentinl <florentinl@users.noreply.github.com>
2024-03-07 22:57:24 +0000

    deploy: 4375d661ff72c212652e5c8d48f07b92bae1bbb6

--- source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"giga":["",[["editor",[["terminal",[],["mod.rs","termion.rs"]],["view",[["file",[],["color.rs","mod.rs"]]],["mod.rs"]]],["command.rs","git.rs","mod.rs","signal.rs"]]],["main.rs"]]\
-}');
-createSourceSidebar();
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["giga",["",[["editor",[["terminal",[],["mod.rs","termion.rs"]],["view",[["file",[],["color.rs","mod.rs"]]],["mod.rs"]]],["command.rs","git.rs","mod.rs","signal.rs"]]],["main.rs"]]]\
+]'));
+createSrcSidebar();
